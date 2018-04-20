329cf9ea2e1a77f98161632d18885120a06aee47
annapasca <a.pascarella@iac.cnr.it>
2018-04-20 11:02:04 +0200

    added test

--- ephypype/compute_inv_problem.py
+++ ephypype/compute_inv_problem.py
@@ -241,7 +241,12 @@ def _compute_inverse_solution(raw_filename, sbj_id, subjects_dir, fwd_filename,
             raise ValueError('For aseg mixed orientation has to be True ***\n')
         print('\n *** is mixed = {} *** \n'.format(is_mixed))
         forward = mne.convert_forward_solution(forward, surf_ori=False,
-                                                   is_mixed=is_mixed)
+                                               is_mixed=is_mixed)
+
+#    TEST!
+#    from scipy.io import savemat
+#    savemat('/media/pasca/paska/meg_data/Tarek/data/Pilote6/TEST_fixed.mat',
+#            {'LF': forward['sol']['data']})
 
     lambda2 = 1.0 / snr ** 2
 
